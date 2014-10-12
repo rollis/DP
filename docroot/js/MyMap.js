b2effe17e906cb7c463c1a550077ad9b58f011d3
Jin Hwang <redbanana.blueapple@gmail.com>
2014-10-12 10:09:54 -0400

    marker requires click

--- docroot/js/MyMap.js
+++ docroot/js/MyMap.js
@@ -1,45 +1,47 @@
 var path=[], venues= [], track=[];
 
 MyMap = function(id){
-$("#"+id).append('<div id="radius_input">\
-  <div><button class="btn-u travel-type" type="button" value="DRIVING" style="margin-right:5px;"><i class="fa fa-car"></i></button><button class="btn-u travel-type" value="WALKING" type="button"><i class="fa fa-paw"></i></button>\
-  </div>\
-  <p><label for="amount">Radius:</label>\
-  <input type="text" id="amount" readonly style="width:100px;border:0; color:#f6931f; font-weight:bold;"></p>\
-  <div id="slider"></div>\
-  <div>\
-  <label for="keyword">Keyword:</label>\
-  <input type="text" id="keyword" value="restaurant" style="margin-bottom:10px;">\
-  </div>\
-  <button class="btn btn-success btn-xs" type="button" id="search"><i class="fa fa-search"></i>Search</button>\
-  </div>');
+  $("#"+id).append('<div id="radius_input">\
+    <div><button class="btn-u travel-type" type="button" value="DRIVING" style="margin-right:5px;"><i class="fa fa-car"></i></button><button class="btn-u travel-type" value="WALKING" type="button"><i class="fa fa-paw"></i></button>\
+    </div>\
+    <p><label for="amount">Radius:</label>\
+    <input type="text" id="amount" readonly style="width:100px;border:0; color:#f6931f; font-weight:bold;"></p>\
+    <div id="slider"></div>\
+    <div>\
+    <label for="keyword">Keyword:</label>\
+    <input type="text" id="keyword" value="restaurant" style="margin-bottom:10px;">\
+    </div>\
+    <button class="btn btn-success btn-xs" type="button" id="search"><i class="fa fa-search"></i>Search</button>\
+    </div>');
 
-//var venues=[];
-var currentVenue = null;
-var drivingtype = google.maps.TravelMode.DRIVING;
+  this.searchResultVenues=[];
+  this.selectedVenues=[];
+  var currentVenue = null;
+  var drivingtype = google.maps.TravelMode.DRIVING;
 
-$(".travel-type").click(function(e){
-  if($(e.target).val() === "WALKING"){
-    drivingtype = google.maps.TravelMode.WALKING;
-  }else{
-    drivingtype = google.maps.TravelMode.DRIVING;    
-  }
-});
+  $(".travel-type").click(function(e){
+    if($(e.target).val() === "WALKING"){
+      drivingtype = google.maps.TravelMode.WALKING;
+    }else{
+      drivingtype = google.maps.TravelMode.DRIVING;    
+    }
+  });
 
-$( "#slider" ).slider({
-      value:500,
-      min: 0,
-      max: 10000,
-      step: 100,
-      slide: function( event, ui ) {
-        $( "#amount" ).val( ui.value + " meter ");
-      }
-    });
-    $( "#amount" ).val($( "#slider" ).slider( "value" ) + " meter" );
+  $( "#slider" ).slider({
+    value:500,
+    min: 0,
+    max: 10000,
+    step: 100,
+    slide: function( event, ui ) {
+      $( "#amount" ).val( ui.value + " meter ");
+    }
+  });
 
-$("#search").click(function(e){
-  $(".my-thumb-icon").remove();
-  var request = {
+  $( "#amount" ).val($( "#slider" ).slider( "value" ) + " meter" );
+  var self = this;
+  $("#search").click(function(e){
+    $(".my-thumb-icon").remove();
+    var request = {
       ajaxStep: "search",
       term: "restaurant",
       latitude: 40.75831,
@@ -47,88 +49,98 @@ $("#search").click(function(e){
       term: $("#keyword").val(),
       radius: $("#amount").val().split(" ")[0],
       limit:20
-  };
+    };
+    getSearchResultFromAPI(request);
+  });
 
-  $.get("http://edwardrockhands.com/Edward_Map/yelp_api.php",request, function(res){
-    var data = $.parseJSON(res);
-    $.each(data, function(index, value){
-      console.log(value);
-      var address = value.location.display_address.toString();
-      new google.maps.Geocoder().geocode({"address": address}, function(results, status) {
-        if(status == google.maps.GeocoderStatus.OK) {
-          lat_long = {
-            latitude: results[0]["geometry"]["location"]["k"],
-            longitude: results[0]["geometry"]["location"]["B"]
-          };
-          var myIcon = L.divIcon({className: 'my-thumb-icon', iconSize: L.point(50, 50), html:"<div class='thumb' id='"+value.id+"' data-lat='"+lat_long.latitude+"' data-lng='"+lat_long.longitude+"'>"+value.html+"</div>"});
-          L.marker([lat_long.latitude, lat_long.longitude], {icon: myIcon}).addTo(map);
-        }
+  function getSearchResultFromAPI(request)
+  {
+    $.get("http://edwardrockhands.com/Edward_Map/yelp_api.php",request, function(res){
+      var data = $.parseJSON(res);
+      $.each(data, function(index, value){
+        self.searchResultVenues.push(value);
+        var address = value.location.display_address.toString();
+        new google.maps.Geocoder().geocode({"address": address}, function(results, status) {
+          if(status == google.maps.GeocoderStatus.OK) {
+            lat_long = {
+              latitude: results[0]["geometry"]["location"]["k"],
+              longitude: results[0]["geometry"]["location"]["B"]
+            };
+            var markerElement = L.divIcon({className: 'my-thumb-icon', iconSize: L.point(50, 50), html:"<div class='counters'><span class='counter-icon'><i class='fa fa-coffee rounded'></i></span><div class='thumb' id='"+value.id+"' data-lat='"+lat_long.latitude+"' data-lng='"+lat_long.longitude+"'>"+value.html+"</div>"});
+            L.marker([lat_long.latitude, lat_long.longitude], {icon: markerElement}).addTo(map);
+          }
+        });
       });
     });
-  });
-});
+  }
 
-var map = new L.Map(id).setView([40.75831, -73.99151], 17);
-
-
-var myIcon = L.divIcon({className: 'current-location-icon', iconSize: L.point(50, 50), html:"<i class='fa fa-child' style='font-size:30px;'></i>"});
-var center = map.getCenter();
-venues.push([[center.lat, center.lng].toString(),'C']);
-L.marker([center.lat, center.lng], {icon: myIcon}).addTo(map);
-
-
-new L.TileLayer(
-  'http://{s}.tiles.mapbox.com/v3/osmbuildings.gm744p3p/{z}/{x}/{y}.png',
-  { attribution: 'Map tiles &copy; <a href="http://mapbox.com">MapBox</a>', maxNativeZoom: 19, maxZoom: 21 }
-).addTo(map);
-
-var osmb = new OSMBuildings(map)
-  .date(new Date(2014, 5, 15, 17, 30))
-  .load()
-  .click(function(id) {
-    console.log('feature id clicked:', id);
-  });
-
-L.control.layers({}, { Buildings: osmb }).addTo(map);
-
-//var path = [];
-path.push(center);
-
-
-
-$(".leaflet-map-pane").on('click', ".thumb", function(e){
-  $("#alert").show();
-  currentVenue = {id:$(e.target).attr('id'), lat:$(e.currentTarget).data('lat'), lng:$(e.currentTarget).data('lng')};
-  console.log(currentVenue);
-  console.log(e.target);
-});
-
-$("#alert .close, #alert .cancel").click(function(e){
-  $("#alert").hide();
-});
-
-$("#add_venue").click(function(e){
-  venues.push([currentVenue.id, 'Y']);
-  console.log(venues);
-  map.panTo(new L.LatLng(currentVenue.lat, currentVenue.lng));
-  $(".my-thumb-icon").remove();
-  $(".current-location-icon").remove();
-
-  currentVenue = null;
-  $("#alert").hide();
-
+  var map = new L.Map(id).setView([40.75831, -73.99151], 17);
 
   var myIcon = L.divIcon({className: 'current-location-icon', iconSize: L.point(50, 50), html:"<i class='fa fa-child' style='font-size:30px;'></i>"});
   var center = map.getCenter();
+  this.selectedVenues.push([[center.lat, center.lng].toString(),'C']);
   L.marker([center.lat, center.lng], {icon: myIcon}).addTo(map);
 
+
+  new L.TileLayer(
+    'http://{s}.tiles.mapbox.com/v3/osmbuildings.gm744p3p/{z}/{x}/{y}.png',
+    { attribution: 'Map tiles &copy; <a href="http://mapbox.com">MapBox</a>', maxNativeZoom: 19, maxZoom: 21 }
+    ).addTo(map);
+
+ 
+ var osmb = new OSMBuildings(map)
+  .date(new Date(2014, 5, 15, 17, 30))
+  .load()
+  .click(function(id) {
+    //console.log('feature id clicked:', id);
+  });
+  L.control.layers({}, { Buildings: osmb }).addTo(map);
+
+  //var path = [];
   path.push(center);
-  if(path.length === 2){    
-    var latlngs = [];
-    var directionsService = new google.maps.DirectionsService();
-    var request = makeRequest(path[0].lat, path[0].lng, path[1].lat, path[1].lng);
-    path.shift();
-    directionsService.route(request, function(result, status) {
+
+  $('.leaflet-map-pane').on('click', '.my-thumb-icon', function() {
+
+    console.log($(this));
+    $('.my-thumb-icon').find('.thumb').hide();
+    $(this).find('.thumb').show();
+  });
+  console.log($('.my-thumb-icon'));
+
+
+  $(".leaflet-map-pane").on('click', ".thumb", function(e){
+    $("#alert").show();
+    currentVenue = {id:$(e.target).attr('id'), lat:$(e.currentTarget).data('lat'), lng:$(e.currentTarget).data('lng')};
+    console.log(currentVenue);
+    console.log(e.target);
+  });
+
+  $("#alert .close, #alert .cancel").click(function(e){
+    $("#alert").hide();
+  });
+
+  $("#add_venue").click(function(e){
+    this.selectedVenues.push([currentVenue.id, 'Y']);
+    console.log(this.selectedVenues);
+    map.panTo(new L.LatLng(currentVenue.lat, currentVenue.lng));
+    $(".my-thumb-icon").remove();
+    $(".current-location-icon").remove();
+
+    currentVenue = null;
+    $("#alert").hide();
+
+
+    var myIcon = L.divIcon({className: 'current-location-icon', iconSize: L.point(50, 50), html:"<i class='fa fa-child' style='font-size:30px;'></i>"});
+    var center = map.getCenter();
+    L.marker([center.lat, center.lng], {icon: myIcon}).addTo(map);
+
+    path.push(center);
+    if(path.length === 2){    
+      var latlngs = [];
+      var directionsService = new google.maps.DirectionsService();
+      var request = makeRequest(path[0].lat, path[0].lng, path[1].lat, path[1].lng);
+      path.shift();
+      directionsService.route(request, function(result, status) {
         if (status === google.maps.DirectionsStatus.OK) {
           result.routes[0].overview_path.forEach(function(item) {
             track
@@ -136,35 +148,35 @@ $("#add_venue").click(function(e){
           });
           L.polyline(latlngs, {color: 'red'}).addTo(map);
         }
-    });
-  }
-});
+      });
+    }
+  });
 
 
-function makeRequest(start_lat, start_long, end_lat, end_long) {
+  function makeRequest(start_lat, start_long, end_lat, end_long) {
     var start = new google.maps.LatLng(start_lat, start_long);
     var end = new google.maps.LatLng(end_lat, end_long);
     var request = {
-        origin: start,
-        destination: end,
-        travelMode: drivingtype
+      origin: start,
+      destination: end,
+      travelMode: drivingtype
     };
 
     return request;
-};
+  };
 
-function requestLatLng(address, callback) {
-  var lat_long;
-  new google.maps.Geocoder().geocode({"address": address}, function(results, status) {
-    if(status == google.maps.GeocoderStatus.OK) {
-      lat_long = {
-        latitude: results[0]["geometry"]["location"]["k"],
-        longitude: results[0]["geometry"]["location"]["B"]
-      };
-      callback(lat_long);
-    }
-  });
-}
+  function requestLatLng(address, callback) {
+    var lat_long;
+    new google.maps.Geocoder().geocode({"address": address}, function(results, status) {
+      if(status == google.maps.GeocoderStatus.OK) {
+        lat_long = {
+          latitude: results[0]["geometry"]["location"]["k"],
+          longitude: results[0]["geometry"]["location"]["B"]
+        };
+        callback(lat_long);
+      }
+    });
+  }
 }
 
 
@@ -172,6 +184,6 @@ MyMap.prototype.getPath = function(){
   return path;
 }
 
-MyMap.prototype.getVenues = function(){
-  return venues;
+MyMap.prototype.getSelectedVenues = function(){
+  return this.selectedVenues;
 }
